ffacb561a1c79d323d74a4714502ea9f6f124e5f
dazzbourgh <dazzbourgh@gmail.com>
2018-10-14 18:54:12 +0300

    sorting of offers

--- src/main/kotlin/zhi/yest/rocketleaguetracker/controller/OfferController.kt
+++ src/main/kotlin/zhi/yest/rocketleaguetracker/controller/OfferController.kt
@@ -1,6 +1,7 @@
 package zhi.yest.rocketleaguetracker.controller
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import zhi.yest.rocketleaguetracker.domain.Filter
@@ -11,7 +12,10 @@ import zhi.yest.rocketleaguetracker.service.OfferService
 @RequestMapping("/offer")
 class OfferController
 @Autowired constructor(private val offerService: OfferService) {
+    @GetMapping
     fun offers(id: Int, searchType: Int, pagesNum: Int): List<Offer> {
-        return offerService.getOffers(Filter(id, 0, 0, searchType), pagesNum)
+        val offers = offerService.getOffers(Filter(id, 0, 0, searchType), pagesNum)
+        return if (searchType == 0) offers.sortedBy { it.has.totalPrice }
+        else offers.sortedBy { it.wants.totalPrice }
     }
 }
